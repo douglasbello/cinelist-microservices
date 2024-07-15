b66774ab31e1f99cd5bcdf170c2d05e8327b158e
Douglas Bello <dgbello.dev@hotmail.com>
2024-07-15 15:12:12 -0300

    feat: initialized search microservice

---
+++ search/src/main/java/com/cinelist/ms/search/SearchApplication.java
@@ -0,0 +1,13 @@
+package com.cinelist.ms.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+
+}
