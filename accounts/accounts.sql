f178f91a9f8ce92061181cd23a1c0a6ec55070ee
Bello <dgbello.dev@hotmail.com>
2024-07-15 22:59:11 -0300

    changes(accounts): switched datasource from h2 to psql

--- accounts/accounts.sql
+++ accounts/accounts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tb_users(
-    pk INT NOT NULL PRIMARY KEY,
+    pk SERIAL NOT NULL PRIMARY KEY,
     identifier UUID NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -7,8 +7,9 @@ CREATE TABLE tb_users(
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     biography VARCHAR(255) DEFAULT NULL,
+	location VARCHAR(255) DEFAULT NULL,
     photo_url VARCHAR(255) DEFAULT NULL,
     birth_date DATE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
 )
