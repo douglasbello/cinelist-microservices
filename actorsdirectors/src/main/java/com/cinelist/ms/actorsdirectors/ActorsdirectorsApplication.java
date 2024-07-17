b6214ff53cb9a5667f9672cc1823b6b6d14eef37
Douglas Bello <dgbello.dev@hotmail.com>
2024-07-17 14:52:38 -0300

    feat: initialized actors/directors microservice

---
+++ actorsdirectors/src/main/java/com/cinelist/ms/actorsdirectors/ActorsdirectorsApplication.java
@@ -0,0 +1,13 @@
+package com.cinelist.ms.actorsdirectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsdirectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsdirectorsApplication.class, args);
+	}
+
+}
