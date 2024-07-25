6afa5850963a5943dc7cb531f2d08937126776ca
Douglas Bello <dgbello.dev@hotmail.com>
2024-07-25 08:37:03 -0300

    fix(catalog): fixing query error in PlatformRepository

--- catalog/src/main/java/com/cinelist/ms/catalog/controllers/search/MovieSearchController.java
+++ catalog/src/main/java/com/cinelist/ms/catalog/controllers/search/MovieSearchController.java
@@ -1,9 +1,4 @@
 package com.cinelist.ms.catalog.controllers.search;
 
-import com.cinelist.ms.catalog.database.models.Movie;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-
 public interface MovieSearchController {
-    ResponseEntity<Page<Movie>> findAll();
 }
--- catalog/src/main/java/com/cinelist/ms/catalog/database/repositories/PlatformRepository.java
+++ catalog/src/main/java/com/cinelist/ms/catalog/database/repositories/PlatformRepository.java
@@ -3,10 +3,11 @@ package com.cinelist.ms.catalog.database.repositories;
 import com.cinelist.ms.catalog.database.models.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface PlatformRepository extends JpaRepository<Platform, UUID> {
-    @Query(nativeQuery = true, value = "SELECT * FROM tb_platforms WHERE name LIKE '%?1%'")
-    Platform findByName(String name);
+    @Query(value = "SELECT p FROM Platform p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    Platform findByName(@Param("name") String name);
 }
