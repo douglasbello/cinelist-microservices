208593f2c93b633d5ae2888ad227844aa58ef3b0
Douglas Bello <dgbello.dev@hotmail.com>
2024-07-29 16:46:16 -0300

    refactoring(cast): deleted useless files

--- actorsdirectors/src/main/java/com/cinelist/ms/actorsdirectors/database/repositories/ActorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cinelist.ms.actorsdirectors.database.repositories;
-
-import com.cinelist.ms.actorsdirectors.database.models.Actor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ActorRepository extends JpaRepository<Actor, UUID> {
-}
--- actorsdirectors/src/main/java/com/cinelist/ms/actorsdirectors/database/repositories/DirectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cinelist.ms.actorsdirectors.database.repositories;
-
-import com.cinelist.ms.actorsdirectors.database.models.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DirectorRepository extends JpaRepository<Director, UUID> {
-}
