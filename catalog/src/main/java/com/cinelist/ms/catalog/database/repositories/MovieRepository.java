d732a9b9ec8e2424b1775b5d08facee8b8d71b00
Douglas Bello <dgbello.dev@hotmail.com>
2024-08-07 14:32:15 -0300

    fix(catalog): fixed latest movies endpoint that was not working properluy

--- catalog/src/main/java/com/cinelist/ms/catalog/database/repositories/MovieRepository.java
+++ catalog/src/main/java/com/cinelist/ms/catalog/database/repositories/MovieRepository.java
@@ -19,6 +19,6 @@ public interface MovieRepository extends JpaRepository<Movie, UUID> {
     Page<Movie> findAllByTitle(@Param("title") String title, Pageable pageable);
     @Query(value = "SELECT m FROM Movie m WHERE m.releaseDate > CURRENT_DATE")
     Page<Movie> upcoming(Pageable pageable);
-    @Query(value = "SELECT m FROM Movie m WHERE m.releaseDate <= CURRENT_DATE")
+    @Query(value = "SELECT m FROM Movie m ORDER BY m.releaseDate DESC")
     Page<Movie> latest(Pageable pageable);
 }
